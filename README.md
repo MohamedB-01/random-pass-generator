90ef9a8907529dc503cb3e9cdc11740467ed4f65
MohamedB <m.m.bassiouni@gmail.com>
2021-04-10 06:16:51 -0700

    updating for intial homework submission

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # random-pass-generator
-create variables
-Display a prompt asking the user for password length
-display confirms for all different character typescheck IF the user answered true to any of the confirms, add that array to the possible character array
-create a loop for as long as passwordLength
-for each iteration, select a random char out of the possible characters array and add it to the final password variable 
-Write password to the #password input
+create variables:
+  lowercase array.
+  uppercase array.
+  special characters array.
+  numbers array.
+  possible characters array.
+  variable to hold confirm responses. 
+
+
+Display confirms for all different character typescheck IF the user answered true to any of the confirms, add that array to the possible character array.
+
+create a function to generate password.
+ create a variable that will hold the final password.
+ Display a prompt asking the user for password length.
+ match edges cases - is a number && is >=8 , <=128.
+ create a loop for as long as passwordLength.
+ for each iteration, select a random char out of the possible characters array and add it to the final password variable. 
+
+Write password to the #password input.
+
+Write password to the #password input.
+
+Add event listener to generate button.
--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var generateBtn = document.querySelector("#generate");
 
 // create variables
  
+
   // lowercase array
   var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
-
   
   // uppercase array
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
@@ -18,7 +18,7 @@ var generateBtn = document.querySelector("#generate");
   var numArr = "0123456789".split("");
  
   // possible characters array
-  possibleChar = []
+  possibleChar = [];
  
   // variable to hold confirm responses 
    var ucChoice = true ;
@@ -26,58 +26,67 @@ var generateBtn = document.querySelector("#generate");
    var numChoice = true ;
    var specCharChoice = true ;
 
-  // create a variable that will hold the final password
-  var password = ""
+ 
 
-  // Display a prompt asking the user for password length
-var passwordLength = prompt ("please enter desired password lenght");
-
-// TODO: match edges cases - is a number && is >=8 , <=128
-  // TODO: ensure that they answered true to at least on confirm
-if (passwordLength >= 8 && passwordLength <= 128 ) {
-console.log(passwordLength)
-} else {
-  alert("you must enter a number between 8 and 28")
-}
+  // display confirms for all different character types (listed in README)
+  function choices(){
+ 
+    var ucChoice = confirm ("do you want password to contain upper case letters?");
+    var numChoice = confirm ("do you want password to contain numbers?");
+    var  specCharChoice = confirm ("do you want your password to contain special characters ?")
   
-// display confirms for all different character types (listed in README)
-var ucChoice = confirm ("do you want password to contain upper case letters?");
-var numChoice = confirm ("do you want password to contain numbers?");
-var  specCharChoice = confirm ("do you want your password to contain special characters ?")
+  
+   // check IF the user answered true to any of the confirms, add that array to the possible character array 
+    if (ucChoice ===true && numChoice === true && specCharChoice === true) {
+      possibleChar = lowercase.concat(uppercase,numArr, specialChar);
+    } else if (ucChoice === true && numChoice === true && specCharChoice === false){
+      possibleChar = lowercase.concat(uppercase, numArr);
+    } else if ( ucChoice === true && numChoice === false && specCharChoice === false) {
+      possibleChar = lowercase.concat(uppercase);
+    } else if ( ucChoice === false && numChoice=== true && specCharChoice===true) {
+      possibleChar = lowercase.concat(numArr, specialChar);
+    } else if (ucChoice === false && numChoice === false && specCharChoice=== true){
+      possibleChar = lowercase.concat(specialChar);
+    } else if ( ucChoice === true && numChoice === false && specCharChoice === true){
+      possibleChar = lowercase.concat(uppercase, specialChar);
+    } else if ( ucChoice === false && numChoice === true && specCharChoice === false){
+      possibleChar = lowercase.concat(numChoice);
+    } else {
+      possibleChar = lowercase;
+    }
+   } ;
 
+  
 
- // check IF the user answered true to any of the confirms, add that array to the possible character array 
-  if (ucChoice ===true && numChoice === true && specCharChoice === true) {
-    possibleChar = lowercase.concat(uppercase,numArr, specialChar);
-  } else if (ucChoice === true && numChoice === true && specCharChoice === false){
-    possibleChar = lowercase.concat(uppercase, numArr);
-  } else if ( ucChoice === true && numChoice === false && specCharChoice === false) {
-    possibleChar = lowercase.concat(uppercase);
-  } else if ( ucChoice === false && numChoice=== true && specCharChoice===true) {
-    possibleChar = lowercase.concat(numArr, specialChar);
-  } else if (ucChoice === false && numChoice === false && specCharChoice=== true){
-    possibleChar = lowercase.concat(specialChar);
-  } else if ( ucChoice === true && numChoice === false && specCharChoice === true){
-    possibleChar = lowercase.concat(uppercase, specialChar);
-  } else if ( ucChoice === false && numChoice === true && specCharChoice === false){
-    possibleChar = lowercase.concat(numChoice);
-  } else {
-    possibleChar = lowercase;
-  }
 
 function generatePassword(){
-  //TODO: your code here
-
+  
+  // create a variable that will hold the final password
+  var password = [];
+  
+  // Display a prompt asking the user for password length
+  var passwordLength = prompt ("please enter desired password lenght");
+  
+//  match edges cases - is a number && is >=8 , <=128
+  
+if (passwordLength >= 8 && passwordLength <= 128 && isNaN(passwordLength) == false) {
+ choices();
   // want to create a loop for as long as passwordLength
   // for each iteration, select a random char out of the possible characters array and add it to the final password variable
-  for (var i = 0; i <= passwordLength; i++) {
-    password =  possibleChar [Math.floor(Math.random() * possibleChar.length -1)]
+  for (var i = 0; i < passwordLength; i++) {
+    // random number var
+    var randomNum = Math.floor(Math.random() * possibleChar.length );
+    password +=  possibleChar[randomNum]; 
   }
+ // return "Password"
+ return password ;
+  
+}  else {
+   alert("you must enter a number between 8 and 128");
 
-  // return the final password from the generatePassword function
- // return "extra Secret Password"
+   };
 
-}
+};
   
 
 
